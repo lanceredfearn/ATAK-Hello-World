0757b1e8d4f74b1ce6d9b8606263664d5150cc3d
Shawn Bisgrove <shawn_bisgrove@partech.com>
2020-01-20 12:37:42 -0500

    + onPress, onUnpress, onMove, onClick example

--- app/src/main/java/com/atakmap/android/helloworld/HelloWorldWidget.java
+++ app/src/main/java/com/atakmap/android/helloworld/HelloWorldWidget.java
@@ -4,6 +4,8 @@ package com.atakmap.android.helloworld;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.view.MotionEvent;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -21,7 +23,7 @@ import android.content.BroadcastReceiver;
 import com.atakmap.coremap.log.Log;
 
 public class HelloWorldWidget extends AbstractWidgetMapComponent implements
-        OnPressListener {
+        OnPressListener, MapWidget.OnUnpressListener, MapWidget.OnMoveListener, MapWidget.OnClickListener {
 
     private MapView mapView;
 
@@ -42,36 +44,62 @@ public class HelloWorldWidget extends AbstractWidgetMapComponent implements
 
     }
 
+    MarkerIconWidget widget = null;
+
     final BroadcastReceiver wacky = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             Log.d(TAG, "wacky search");
 
-            MarkerIconWidget widget = new MarkerIconWidget();
+            if (widget == null) {
+                widget = new MarkerIconWidget();
 
-            String imageUri = "android.resource://"
-                    + mapView.getContext().getPackageName() + "/"
-                    + com.atakmap.app.R.drawable.sync_search;
-            widget.setPoint((mapView.getWidth() / 2)
-                    - (MapView.DENSITY * ICON_WIDTH / 2) + 72, 80f);
+                String imageUri = "android.resource://"
+                        + mapView.getContext().getPackageName() + "/"
+                        + com.atakmap.app.R.drawable.sync_search;
+                widget.setPoint((mapView.getWidth() / 2)
+                        - (MapView.DENSITY * ICON_WIDTH / 2) + 72, 160f);
 
-            Icon.Builder builder = new Icon.Builder();
-            builder.setAnchor(0, 0);
-            builder.setColor(Icon.STATE_DEFAULT, Color.WHITE);
-            builder.setSize(ICON_WIDTH, ICON_HEIGHT);
-            builder.setImageUri(Icon.STATE_DEFAULT, imageUri);
+                Icon.Builder builder = new Icon.Builder();
+                builder.setAnchor(0, 0);
+                builder.setColor(Icon.STATE_DEFAULT, Color.WHITE);
+                builder.setSize(ICON_WIDTH, ICON_HEIGHT);
+                builder.setImageUri(Icon.STATE_DEFAULT, imageUri);
 
-            Icon icon = builder.build();
-            widget.setIcon(icon);
+                Icon icon = builder.build();
+                widget.setIcon(icon);
 
-            widget.addOnPressListener(HelloWorldWidget.this);
+                widget.addOnClickListener(HelloWorldWidget.this);
+                widget.addOnPressListener(HelloWorldWidget.this);
+                widget.addOnUnpressListener(HelloWorldWidget.this);
+                widget.addOnMoveListener(HelloWorldWidget.this);
+
+            }
             if (!getRootLayoutWidget().getChildWidgets().contains(widget))
                 getRootLayoutWidget().addWidget(widget);
         }
     };
 
+    private PointF pointDown = null;
+    private boolean dragging;
+
+
     @Override
-    public void onMapWidgetPress(MapWidget widget, MotionEvent event) {
+    public boolean onMapWidgetMove(MapWidget widget, MotionEvent event) {
+        if (dragging || (pointDown != null &&
+            Math.abs(pointDown.x - event.getX()) < 2 &&
+                Math.abs(pointDown.y - event.getY()) < 2)) {
+            dragging = true;
+        }
+
+
+        Log.d(TAG, event.getX() + " " + event.getY());
+        return true;
+    }
+
+    @Override
+    public void onMapWidgetClick(MapWidget widget, MotionEvent event) {
+        Log.d(TAG, "onClickListener called");
         AlertDialog.Builder builderSingle = new AlertDialog.Builder(
                 mapView.getContext());
         builderSingle.setTitle("Wacky Search");
@@ -88,7 +116,28 @@ public class HelloWorldWidget extends AbstractWidgetMapComponent implements
     }
 
     @Override
+    public void onMapWidgetUnpress(MapWidget widget, MotionEvent event) {
+        Log.d(TAG, "onMapUnpressed called");
+    }
+
+
+    @Override
+    public void onMapWidgetPress(MapWidget widget, MotionEvent event) {
+        if (dragging) {
+            pointDown = null;
+            dragging = false;
+            return;
+        }
+
+        Log.d(TAG, "onMapPressed called");
+        pointDown = new PointF(event.getX(), event.getY());
+    }
+
+    @Override
     protected void onDestroyWidgets(Context context, MapView view) {
+        if (widget != null)
+            getRootLayoutWidget().removeWidget(widget);
+
     }
 
     @Override
